777b84b4b13c342b167ebc979f7db49b7c56fdee
Shugaev <alek-2020@yande.ru>
2018-08-19 16:27:10 +0300

    Кастомная иконка для станций

--- src/mappers.js
+++ src/mappers.js
@@ -5,23 +5,33 @@ export function mapServerData(serverData) {
       id: index,
       type: "Feature",
       isActive: obj.isActive,
-      geometry: 
-      {
+      geometry: {
         type: "Point",
         coordinates: [obj.lat, obj.long]
       },
       properties: {
-        iconCaption: obj.serialNumber
+        iconCaption: obj.serialNumber,
       },
       options: {
-        preset: getObjectPreset(obj)
+        iconLayout: 'default#image',
+        iconImageSize: [30, 30],
+        iconImageHref: getIconHref(obj),
+        iconColor: getColor(obj),
+        // Сместим иконку, что бы она была точно посередине и балунт открывался на ней
+        iconImageOffset: [-14, -15]
       }
     }))
   };
 }
 
-function getObjectPreset(obj) {
-  return obj.isActive
-    ? 'islands#blueCircleDotIconWithCaption'
-    : 'islands#redCircleDotIconWithCaption';
+function getColor(obj) {
+  return obj.isActive ?
+    '#1E98FF' :
+    '#ED4543';
+}
+
+function getIconHref(obj) {
+  return obj.isActive ?
+    'https://res.cloudinary.com/dpjuglsyv/image/upload/v1534681338/active-station-icon.svg' :
+    'https://res.cloudinary.com/dpjuglsyv/image/upload/v1534681337/break-station-icon.svg';
 }
